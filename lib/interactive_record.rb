f277543f53d745ce8f75011d50832d1b2cdf694e
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 21:54:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
   end
 
-  def self.find_by(name:)
+  def self.find_by(name:, grade:)
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
   end
 
